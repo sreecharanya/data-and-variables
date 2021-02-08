005ac919bc3386728e26dd3f0bb0a7cc04f07461
Sreecharanya Srinivasan <sree3charanya@gmail.com>
2021-02-08 01:46:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("line 1", typeof '17')
+console.log("line 2", typeof 17)
+console.log("line 3", "Bruce's beard");
+console.log("line 4", 'The knights who say "Ni!"')
+console.log("line 5" , 'The knights who say "Ni!"')
+console.log("line 6", 420)
+console.log("line 7", 42,000)
+console.log("line 8", 42, 17, 56, 34, 11, 4.35, 32);
+let hello='hi';
+console.log("line 10", 3.4, hello, 45);
+console.log('line 11', typeof false);
+console.log("line 12", Number("three"));
+console.log("line 13", Number('3 3'));
+let day = "Thursday";
+ console.log("line 15",day);
+
+ day = "Friday";
+ console.log("line 18",day);
+
+ day = 21;
+ console.log("line21",day);
+ //const appName = "Get It Done";
+//appName = "Best TODO application Ever!";
+let x = 5;
+const y = 3.14;
+let z = y * x ** 2;
+console.log("line 27", z);
+console.log("line 28", 16 - 2 * 5 / 3 + 1)
+console.log("line 29", 2 ** 2 ** 3 * 3)
+console.log("line 30",1 + 5 % 3);
+const input = require('readline-sync');
+
+let info = input.question("line 33,Enter a name? ");
+console.log("line 34", info)
+
+let info1 = input.question("Please enter your age: ");
+//The user enters 25.
+console.log("line 38",typeof Number(info1));
